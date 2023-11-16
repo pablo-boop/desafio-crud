a5eb96d6fb62a8ee2c26bb6c33ad6bf213a07951
Pablo Couto <pablocouto@aluno.senai.br>
2023-11-16 08:59:53 -0300

    feat: made animals routes

--- src/routes/animals.routes.js
+++ src/routes/animals.routes.js
@@ -1,1 +1,17 @@
 import { Router } from "express";
+
+import {
+    getAnimals,
+    getAnimalById,
+    addAnimal,
+    updateAnimal,
+    deleteAnimal
+} from '../controllers/animals.controlles.js';
+
+const animalsRouter = Router();
+
+animalsRouter.get("/", getAnimals);
+animalsRouter.get("/:id", getAnimalById);
+animalsRouter.post("/", addAnimal);
+animalsRouter.put("/:id", updateAnimal);
+animalsRouter.delete("/:id", deleteAnimal);
